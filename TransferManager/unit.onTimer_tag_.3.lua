34945c68920f3104334b2157fa1c58e57eec87d8
Chris McCullough <ccmccull@yahoo.com>
2022-10-13 12:02:47 -0700

    update

--- TransferManager/unit.onTimer_tag_.3.lua
+++ TransferManager/unit.onTimer_tag_.3.lua
@@ -1,1 +1,7 @@
 sInputContainer1.updateContent()
+local contents = sInputContainer1.getContent()
+if(contents) then
+  DebugPrint(">>>> Adding contents to databank: ")
+  sDataBank.setStringValue("Container.Merged.Content", json.encode(contents))
+  DebugPrint("<<<< Ended databank update")
+end
